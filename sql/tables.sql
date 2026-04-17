bcc5d75d6757a16b70c502aa6c9f9efc3d51c923
laila <lailabahij13@gmail.com>
2026-04-17 17:15:55 +0100

    vérification

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,28 +1,18 @@
-DROP TABLE IF EXISTS transactions CASCADE;
-DROP TABLE IF EXISTS comptes CASCADE;
-DROP TABLE IF EXISTS clients CASCADE;
-DROP TABLE IF EXISTS produits CASCADE;
-DROP TABLE IF EXISTS agences CASCADE;
-DROP TABLE IF EXISTS segments CASCADE;
-DROP TABLE IF EXISTS temps CASCADE;
--- TABLE temps
-CREATE TABLE IF NOT EXISTS temps (
-    id_temps INTEGER PRIMARY KEY,
+CREATE TABLE temps (
+    id_temps SERIAL PRIMARY KEY,
     date_transaction DATE NOT NULL UNIQUE,
-    annees INTEGER NOT NULL,
+    annee INTEGER NOT NULL,
     mois INTEGER NOT NULL,
     trimestre INTEGER NOT NULL CHECK (trimestre BETWEEN 1 AND 4),
     jour_semaine TEXT NOT NULL
 );
 
--- TABLE segments
-CREATE TABLE IF NOT EXISTS segments (
-    id_segment INTEGER PRIMARY KEY,
+CREATE TABLE segments (
+    id_segment SERIAL PRIMARY KEY,
     nom_segment TEXT NOT NULL UNIQUE
 );
 
--- TABLE clients
-CREATE TABLE IF NOT EXISTS clients (
+CREATE TABLE clients (
     id_client TEXT PRIMARY KEY,
     score_credit INTEGER NOT NULL,
     categorie_risque TEXT CHECK (categorie_risque IN ('Medium','Low','High')),
@@ -31,29 +21,26 @@ CREATE TABLE IF NOT EXISTS clients (
     FOREIGN KEY (id_segment) REFERENCES segments(id_segment)
 );
 
--- TABLE comptes
-CREATE TABLE IF NOT EXISTS comptes (
-    id_compte TEXT PRIMARY KEY,
+CREATE TABLE comptes (
+    id_compte SERIAL PRIMARY KEY,
     id_client TEXT NOT NULL,
     solde REAL NOT NULL,
     FOREIGN KEY (id_client) REFERENCES clients(id_client)
 );
 
--- TABLE produits
-CREATE TABLE IF NOT EXISTS produits (
-    id_produit INTEGER PRIMARY KEY,
+
+CREATE TABLE produits (
+    id_produit SERIAL PRIMARY KEY,
     nom_produit TEXT NOT NULL,
     categorie TEXT NOT NULL
 );
 
--- TABLE agences
-CREATE TABLE IF NOT EXISTS agences (
-    id_agence INTEGER PRIMARY KEY,
+CREATE TABLE agences (
+    id_agence SERIAL PRIMARY KEY,
     nom_agence TEXT NOT NULL
 );
 
--- TABLE transactions
-CREATE TABLE IF NOT EXISTS transactions (
+CREATE TABLE transactions (
     id_transactions TEXT PRIMARY KEY,
     id_compte TEXT NOT NULL,
     id_produit INTEGER NOT NULL,
