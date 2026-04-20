6265474f8e4497d66ad2d4353af512af8b112cdf
laila <lailabahij13@gmail.com>
2026-04-20 10:40:38 +0100

    validation

--- sql/conflict.sql
+++
@@ -1,8 +0,0 @@
--- =========================
--- INSERT DATA: SEGMENTS
--- =========================
-INSERT INTO segments (id_segment, nom_segment) VALUES
-(1, 'VIP'),
-(2, 'Standard'),
-(3, 'Risk')
-ON CONFLICT (id_segment) DO NOTHING;
